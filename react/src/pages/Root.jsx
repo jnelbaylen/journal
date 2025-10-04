64544c1905052131dc10d8de90195396aa5016b1
JohnnelBaylen <jnelbaylen@gmail.com>
2025-10-04 08:57:38 +0800

    chore: clean folder

---
+++ react/src/pages/Root.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import '../styles/global.css'
+
+function Root() {
+
+  return (
+    <>
+  
+    </>
+  )
+}
+
+export default Root
