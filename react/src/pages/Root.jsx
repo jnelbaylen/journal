416b09f7e54b20ea431f48ba48aed5378b4d62ca
JohnnelBaylen <jnelbaylen@gmail.com>
2025-10-04 17:23:57 +0800

    chore: install libraries and arrange import link

--- react/src/pages/Root.jsx
+++ react/src/pages/Root.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-import '../styles/global.css'
 
 function Root() {
 
   return (
     <>
-  
+      <h1>Loading.....</h1>
     </>
   )
 }
