25f1210b5a309a0865b8c7ef12e54c8ccc81cf4a
JohnnelBaylen <jnelbaylen@gmail.com>
2025-10-11 07:15:47 +0800

    chore: add boolean and float

--- python/basic/theory/books/variables.py
+++ python/basic/theory/books/variables.py
@@ -1,6 +1,10 @@
 user_full_name = "John Doe"
 user_age = 25
+user_money = 100.34
+user_is_trusted = True
 
-print(user_full_name)
-print(user_age)
+print('Full Name : ', user_full_name)
+print('Age : ', user_age)
+print('Money : ', user_money)
+print('Trusted : ', user_is_trusted)
 
