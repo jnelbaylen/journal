625917ba4a2a7bc159ef93548d7a302605b28ed2
JohnnelBaylen <jnelbaylen@gmail.com>
2025-10-27 21:13:31 +0800

    chore: import file and create greeting function

--- react/src/pages/Root.jsx
+++ react/src/pages/Root.jsx
@@ -1,10 +1,19 @@
 import MyButton from "../components/Button/MyButton";
+import AboutPage from "./About/AboutPage";
+import MyProfile from "../components/Profile/MyProfile";
+
+function Greeting({ name }) {
+  return <h1>Hello, {name}</h1>
+}
 
 export default function Root() {
   return (
     <>
       <h1>Welcome to app</h1>
       <MyButton />
+      <AboutPage />
+      <Greeting name="World" />
+      <MyProfile />
     </>
   )
 }
