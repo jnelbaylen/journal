000cd212ad9069d9d9e61d6b40138a13672473bd
JohnnelBaylen <jnelbaylen@gmail.com>
2025-10-28 10:00:48 +0800

    chore: implement counter state and handler function

--- react/src/components/Button/MyButton.jsx
+++ react/src/components/Button/MyButton.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
+
 export default function MyButton() {
+
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        alert('Hello, there');
+    }
+
+    function handleMessageCount() {
+        setCount(count + 1)
+        console.log("Counting...")
+    }
+
     return (
-        <button>I'm a button</button>
+        <div>
+            <button className="btn-one" onClick={handleClick}>I'm a button</button>
+            <button className="btn-two" onClick={handleMessageCount}>Clicked {count} times</button>
+        </div>
     )
 }
 
